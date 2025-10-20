99dfaca0cfc588e0c3d1a6fe2e275824a7890d5a
Bassline32 <ivandzen911@Gmail.com>
2025-10-20 22:21:38 +0300

    Последовательное вычисление + Выход из программы

--- src/Main.java
+++ src/Main.java
@@ -4,51 +4,60 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите число");
-        double num1 = scanner.nextDouble();
+        double middleResult = 0.0;
 
-        boolean validInput = false;
-        while (!validInput) {
-            System.out.println("Введите второе число");
-            double num2 = scanner.nextDouble();
+        System.out.println("Введите первое число");
+        middleResult = scanner.nextDouble();
 
-            System.out.println("Какую операцию Вы хотите сделать?");
-            System.out.println(" 1(сложение) 2(вычетание) 3(умножение) 4(деление)");
-            int choice = scanner.nextInt();
+      boolean keepCalculating = true;
+      while (keepCalculating) {
+          System.out.println("Какую операцию Вы хотите сделать?");
+          System.out.println("1(+) 2(-) 3(*) 4(/) 5(Выйти из программы)");
+          int choiсe = scanner.nextInt();
 
-            switch (choice) {
-                case 1:
-                    double sum = num1 + num2;
-                    System.out.println("сумма" + sum);
-                    validInput = true;
-                    break;
+          if(choiсe == 5) {
+              System.out.println("Вы вышли из программы");
+              break;
+          }
 
-                case 2:
-                    double minus = num1 - num2;
-                    System.out.println("разность" + minus);
-                    validInput = true;
-                    break;
+          System.out.println("Введите второе число");
+          double num2 = scanner.nextDouble();
 
-                case 3:
-                    double mnojit = num1 * num2;
-                    System.out.println("произведение" + mnojit);
-                    validInput = true;
-                    break;
+          switch (choiсe) {
+              case 1:
+               middleResult = middleResult + num2;
+                  System.out.println("сумма" + middleResult);
+                  break;
 
-                case 4:
-                    if (num2 != 0) {
-                        double delit = num1 / num2;
-                        System.out.println("деление" + delit);
-                        validInput = true;
-                    } else {
-                        System.out.println("  ну кто же делит на нуль. Давай попробуем ещё раз");
-                    }
-                    break;
+              case 2:
+                  middleResult = middleResult - num2;
+                  System.out.println("разность" + middleResult);
+                  break;
 
-                default:
-                    System.out.println("Вы выбрали неверную операцию. Повторите выбор.");
-                    break;
-            }
-        }
+              case 3:
+                  middleResult = middleResult * num2;
+                  System.out.println("произведение" +middleResult);
+                  break;
+
+              case 4:
+                  if (num2 != 0) {
+                      middleResult = middleResult / num2;
+                      System.out.println("деление" + middleResult);
+                  } else {
+                      System.out.println("  ну кто же делит на нуль. Давай попробуем ещё раз");
+                  }
+                  break;
+
+              default:
+                  System.out.println("Вы выбрали неверную операцию. Повторите выбор.");
+                  break;
+          }
+      }
     }
-    }
+
+      }
+
+
+
+
+
---
+++ src/OperationHistory.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OperationHistory {
+private final List<String> operations= new ArrayList<>();
+
+
+
+
+
+
+
+    public List<String> getOperations() {
+        return operations;
+    }
+}
