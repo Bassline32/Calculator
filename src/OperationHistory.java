4967be9e4783ca7cdc0f9e7d0b6c6698affec4b6
Bassline32 <ivandzen911@Gmail.com>
2025-10-21 17:09:26 +0300

    Класс, в котором создан список всех вычислений. Добавлен методы для записи и отображения истории вычислений

--- src/OperationHistory.java
+++ src/OperationHistory.java
@@ -2,15 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OperationHistory {
-private final List<String> operations= new ArrayList<>();
+private List<String> recordOperation; //создали образ списка
 
-
-
-
-
-
-
-    public List<String> getOperations() {
-        return operations;
-    }
+public OperationHistory() {
+    recordOperation = new ArrayList<>(); // выделили списку память
 }
+
+// метод для добавления оперций в историю
+
+public void addRecord(String operationName, double result) {
+    recordOperation.add(operationName + result);
+}
+
+//метод для вывода списка
+
+    public void printHistory() {
+        System.out.println("История Ваших вычислений");
+        for (String entry : recordOperation){
+            System.out.println(entry);
+        }
+    }
+
+ }
+
+
+
